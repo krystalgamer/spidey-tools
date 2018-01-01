f70ba23cdf479cb61a82fa8d5292d5c7a4f372e1
krystalgamer <callofdutypsp@gmail.com>
2018-01-01 12:28:07 +0000

    Gcc is now forwarding binkw32 calls

--- texture_loader/converter.py
+++ texture_loader/converter.py
@@ -3,7 +3,6 @@ import re
 with open('forwards.txt') as f:
     for line in f:
         if len(line.split()) != 0:
-            funcName, ordinal = line.split(':')[1].split('@')[0], line.split(':')[1].split('@')[1]
+            funcName, ordinal = line.split(':')[1].split('=')[0], line.split('@')[3].split('\"')[0]
 
-
-            print(funcName + ' lol ' + ordinal)
+            print('FORWARDED_EXPORT_WITH_ORDINAL('+funcName+','+ordinal+',binkw32_.'+funcName+')')
--- texture_loader/texture_loader.c
+++ texture_loader/texture_loader.c
@@ -1,27 +1,29 @@
 #include "patches.h"
+#include "forwards.h"
 
 BOOL ApplyHooks();
 
 BOOL WINAPI DllMain(HINSTANCE hInstDll, DWORD fdwReason, LPVOID reserverd){
+
 	if(fdwReason == DLL_PROCESS_ATTACH){
 
+		
 		//Console is now allocated only with -console directive
 		AllocConsole();
-		if(!freopen("COUNOUT$", "w", stdout)){
+		if(!freopen("CONOUT$", "w", stdout)){
 			MessageBoxA(NULL, "Error", "Couldn't allocate console...Closing", 0);
 			return FALSE;
 		}
 
-		//Not really
-		char *args = GetCommandLine();
+	//	//Not really
+	//	char *args = GetCommandLine();
 		if (!GetModuleHandle("binkw32.dll")){
 			printf("Original binkw32 doesn't seem to be loaded...Quitting");
-			MessageBoxA(NULL, "Error", "Coudln't get an handle to the original binkw32", 0);
+			MessageBoxA(NULL, "Error", "Couldn't get an handle to the original binkw32", 0);
 			return FALSE;
 		}
 
-		return ApplyHooks();
-		NopMemory()
+		//return ApplyHooks();
 	}
 
 	return TRUE;
@@ -30,6 +32,5 @@ BOOL WINAPI DllMain(HINSTANCE hInstDll, DWORD fdwReason, LPVOID reserverd){
 
 BOOL ApplyHooks(){
 
-	return (ApplyHooks());
-
+	return (DisableIntros());
 }
